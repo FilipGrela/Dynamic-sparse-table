5e25c8942c967599f391653a26e6f0d53a3c5dc9
Filip Grela <56220954+FilipGrela@users.noreply.github.com>
2025-05-25 18:57:02 +0200

    Fix print
    Discovered insert has bug

--- src/Trie.h
+++ src/Trie.h
@@ -1,94 +1,94 @@
-//
-// Created by fgrel on 25/05/2025.
-//
+    //
+    // Created by fgrel on 25/05/2025.
+    //
 
-#ifndef TRIE_H
-#define TRIE_H
+    #ifndef TRIE_H
+    #define TRIE_H
 
-#include "TrieNode.h"
+    #include "TrieNode.h"
 
-template <typename T>
-class Trie {
-private:
+    template <typename T>
+    class Trie {
+    private:
 
-public:
-    size_t rootSize, childSize;
-    TrieNode<T> *root;
+    public:
+        size_t rootSize, childSize;
+        TrieNode<T> *root;
 
-    Trie(const size_t n, const size_t k) : root(new TrieNode<T>(n)) {
-        rootSize = n;
-        childSize = k;
+        Trie(const size_t n, const size_t k) : root(new TrieNode<T>(n)) {
+            rootSize = n;
+            childSize = k;
 
-    }
-
-    /**
-     * Inserts a value into the trie.
-     *
-     * @param value The value to insert into the trie.
-     * @return true if the value was successfully inserted, false if it already exists.
-     */
-    bool insert(T value) {
-        T originalValue = value;
-        if (root->getValue() == 0) {
-            // Jeśli korzeń jest pusty, ustawiamy wartość
-            root->setValue(value);
-            return true;
         }
 
-        TrieNode<T> *currentNode = root;
-        size_t index = value % rootSize; // Reszta z dzielenia przez rootSize
-
-        while (true) {
-            TrieNode<T> *nextNode = currentNode->getChild(index);
-
-            if (nextNode == nullptr) {
-                // Tworzymy nowy węzeł, jeśli nie istnieje
-                nextNode = new TrieNode<T>(childSize);
-                nextNode->setValue(originalValue);
-                currentNode->setChild(index, nextNode);
+        /**
+         * Inserts a value into the trie.
+         *
+         * @param value The value to insert into the trie.
+         * @return true if the value was successfully inserted, false if it already exists.
+         */
+        bool insert(T value) {
+            T originalValue = value;
+            if (root->getValue() == 0) {
+                // Jeśli korzeń jest pusty, ustawiamy wartość
+                root->setValue(value);
                 return true;
             }
 
-            if (nextNode->contains(value)) {
-                // Wartość już istnieje
-                return false;
+            TrieNode<T> *currentNode = root;
+            size_t index = value % rootSize; // Reszta z dzielenia przez rootSize
+
+            while (true) {
+                TrieNode<T> *nextNode = currentNode->getChild(index);
+
+                if (nextNode == nullptr) {
+                    // Tworzymy nowy węzeł, jeśli nie istnieje
+                    nextNode = new TrieNode<T>(childSize);
+                    nextNode->setValue(originalValue);
+                    currentNode->setChild(index, nextNode);
+                    return true;
+                }
+
+                if (nextNode->contains(value)) {
+                    // Wartość już istnieje
+                    return false;
+                }
+
+                currentNode = nextNode;
+
+                value /= childSize;
+                index = value % childSize;
             }
-
-            currentNode = nextNode;
-
-            value /= childSize;
-            index = value % childSize;
         }
-    }
 
 
-    /**
-     * Searches for node that should contain the value.
-     * @param value value to search for.
-     * @return node containing the value or nullptr if not found.
-     */
-    TrieNode<T>* search(T value) {
-        TrieNode<T> *currentNode = root; // Start from the root node
+        /**
+         * Searches for node that should contain the value.
+         * @param value value to search for.
+         * @return node containing the value or nullptr if not found.
+         */
+        TrieNode<T>* search(T value) {
+            TrieNode<T> *currentNode = root; // Start from the root node
 
-        int index = value % rootSize;
-        while (currentNode != nullptr) {
-            if (currentNode->contains(value)) {
-                return currentNode;
+            int index = value % rootSize;
+            while (currentNode != nullptr) {
+                if (currentNode->contains(value)) {
+                    return currentNode;
+                }
+
+
+                currentNode = currentNode->getChild(index);
+                value /= childSize; // Dzielimy wartość przez childSize
+                index = value % childSize; // Obliczamy resztę z dzielenia przez childSize
+
             }
-
-
-            currentNode = currentNode->getChild(index);
-            value /= childSize; // Dzielimy wartość przez childSize
-            index = value % childSize; // Obliczamy resztę z dzielenia przez childSize
-
+            return nullptr;
         }
-        return nullptr;
-    }
 
-    void print() const {
-        root->print();
-        std::cout << std::endl;
-    }
-};
+        void print() const {
+            root->print();
+            std::cout << std::endl;
+        }
+    };
 
-#endif //TRIE_H
+    #endif //TRIE_H
--- src/TrieNode.h
+++ src/TrieNode.h
@@ -6,21 +6,22 @@
 #define TRIENODE_H
 
 
-template <typename T>
+template<typename T>
 class TrieNode {
 private:
     T value;
     TrieNode **children;
     size_t childSize;
+
 public:
-    TrieNode(size_t s) : value(0), children(new TrieNode*[s]), childSize(s) {
+    TrieNode(size_t s) : value(0), children(new TrieNode *[s]), childSize(s) {
         for (size_t i = 0; i < s; i++) {
             children[i] = nullptr;
         }
     }
 
     ~TrieNode() {
-        for (size_t i = 0; i < sizeof(children) / sizeof(children[0]); i++) {
+        for (size_t i = 0; i < childSize; i++) {
             delete children[i];
         }
         delete[] children;
@@ -34,7 +35,7 @@ public:
         this->value = value;
     }
 
-    const T& getValue() const {
+    const T &getValue() const {
         return this->value;
     }
 
@@ -43,22 +44,23 @@ public:
     };
 
     void setChild(size_t index, TrieNode *child) {
-          if (children[index] != nullptr) {
-              delete children[index];
-          }
-          children[index] = child;
-      }
+        if (children[index] != nullptr) {
+            delete children[index];
+        }
+        children[index] = child;
+    }
 
-    void print() const {
-        std::cout << value << " ";
+    void print(size_t index = 0) {
+        printf("(%d, %zu) ", value, index);
+        printf("%d ", value);
+
         for (size_t i = 0; i < childSize; i++) {
             if (children[i] != nullptr) {
-                children[i]->print();
+                children[i]->print(i); // Rekurencyjnie przechodzimy do dzieci
             }
         }
     }
 };
-
 
 
 #endif //TRIENODE_H
